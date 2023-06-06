e28c67ce99f2d60e8fbabc414c3a3c0d804745ae
Crayler <110441088+Crayler@users.noreply.github.com>
2023-06-06 17:10:48 +0800

    Add files via upload

---
+++ name.py
@@ -0,0 +1,28 @@
+# 修改大小写
+name = "ada Love"
+print(name.title())
+# title()将首字母大写
+print(name.upper())
+# upper()所以字母大写
+print(name.lower())
+# lower()所有字母小写
+
+
+# 拼接字符串
+first_name = "Crayler"
+last_name = "Dawn"
+full_name = first_name + "" + last_name
+print(full_name)
+
+print("Hello," + full_name.upper()+ "!")
+
+
+# 制表符和换行符
+print("python")
+print("\tpython")
+print("Languages:\npython\nC\nJava")
+
+
+
+
+
